b66c260ed6342edb42c112c8298ee4d2653e6a9f
Hozmel Roberto de la Rosa Rendón <hozdelarosar@gmail.com>
2026-08-03 16:32:20 -0600

    chore(backend): enable CORS for local dev origins

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,10 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+  });
+
   app.enableShutdownHooks();
 
   app.setGlobalPrefix('api');
