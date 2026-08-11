106196ee7b86f9bd2f818eff6a0d40739eccd39a
Hozmel Roberto de la Rosa Rendón <hozdelarosar@gmail.com>
2026-08-11 00:20:10 -0600

    chore(backend): configure session env and add security dependencies

--- apps/backend/src/config/environment.validation.ts
+++ apps/backend/src/config/environment.validation.ts
@@ -4,9 +4,7 @@ export const environmentValidationSchema = Joi.object({
   NODE_ENV: Joi.string()
     .valid('development', 'test', 'production')
     .default('development'),
-
   PORT: Joi.number().default(3000),
-
   DATABASE_URL: Joi.string()
     .uri({ scheme: ['mysql'] })
     .required(),
